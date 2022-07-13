ed5425e6cb8dc8330fa0e2b78d8768c6403b2847
remishn <nsarem65@gmail.com>
2022-07-12 23:00:05 -0500

    changed to PORT

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const { notes } = require('./db/db.json')
 const express = require('express')
+
+const PORT = process.env.PORT || 3001
 const app = express()
 
 app.get('/api/notes', (req, res) => {
     res.send(notes)
 })
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001`)
 })
