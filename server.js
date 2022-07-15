eecdb1afecc38869d20a9133d56c0f0e0b6439e4
remishn <nsarem65@gmail.com>
2022-07-14 23:35:18 -0500

    get/notes and get created

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ const express = require('express')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+app.use(express.static(__dirname + '/'))
 
 app.get('/api/notes', (req, res) => {
     res.send(notes)
 })
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname })
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile('notes.html', {root: __dirname })
+})
+
 app.listen(PORT, () => {
     console.log(`API server now on port 3001`)
 })
