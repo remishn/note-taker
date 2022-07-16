744bb5ce18611a051b19a7fc8d1211c601af2d5f
remishn <nsarem65@gmail.com>
2022-07-16 18:50:27 -0500

    created post function

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const { notes } = require('./db/db.json')
+const fs = require('fs')
+const path = require('path')
+const { v4: uuidv4 } = require('uuid')
+const { notes }  = require('./db/db.json')
 const express = require('express')
 
 const PORT = process.env.PORT || 3001
 const app = express()
+// link index html
 app.use(express.static(__dirname + '/'))
+
+// parse incoming string or array data 
+app.use(express.urlencoded({ extended: true}))
+// parse incoming JSON data
+app.use(express.json())
 
 app.get('/api/notes', (req, res) => {
     res.send(notes)
@@ -15,6 +24,30 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
     res.sendFile('notes.html', {root: __dirname })
+})
+
+app.post('/api/notes', (req, res) => {
+
+    // collect note from req body
+    let newNote = req.body
+
+    let jsonFilePath = path.join(__dirname, '/db/db.json')
+
+    // assign unique id from npm package
+    newNote.id = uuidv4()
+
+    // push new notes to db.json
+    notes.push(newNote)
+
+    // write notes to db.json
+    fs.writeFile(jsonFilePath, JSON.stringify(notes), function (err)  {
+        if (err) {
+            return console.log(err)
+        } console.log('notes saved')
+    })
+    
+    // send new notes
+    res.json(newNote)
 })
 
 app.listen(PORT, () => {
